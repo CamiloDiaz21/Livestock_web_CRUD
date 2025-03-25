924ae4ec24e7634c912af14094b4a351f8731193
kamilodiaz0521@gmail.com <kamilodiaz0521@gmail.com>
2025-03-25 15:49:47 -0500

    resolvi un pequeño problema (elimine el main)

---
+++ DATOS_VENTAS/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	_ "github.com/sena_2824182/livestock_web_CRUD/DATOS_VENTAS/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
